e354f09003d1f65d32903a9360a2cab99eec413f
André Giampaolo <andregiampaolo@gmail.com>
2019-03-31 20:18:23 -0300

    Add connection to mongodb

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
+mongoose.connect('mongodb+srv://twitter-user:58C22bb794uZ78V@cluster0-pnv1w.mongodb.net/test?retryWrites=true', {
+    useNewUrlParser: true
+});
 
 app.get('/', (req, res) => {
     console.log(req);
     console.log(res);
-    return res.send('Hello world');
+    return res.send('Hello world.');
 });
 
 app.listen(3000,() => {
