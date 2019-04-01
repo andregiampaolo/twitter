6e915d1eb36f66b97b3692bcf89d6a4c7fa69b45
André Giampaolo <andregiampaolo@gmail.com>
2019-03-31 21:58:23 -0300

    Add two routes for list and create tweets

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const routes = express.Router();
 
+const TweetController = require('./controllers/TweetController');
+
+routes.get('/tweets', TweetController.index);
+routes.post('/tweets', TweetController.store);
+
+
 routes.get('/', (req, res) => {
     console.log(req);
     console.log(res);
     return res.send('Hello World!');
 });
+
 
 module.exports = routes;
