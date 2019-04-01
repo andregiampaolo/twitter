d801b5b9a6867a6d512a1b4bdd197d28c8a557c2
André Giampaolo <andregiampaolo@gmail.com>
2019-04-01 00:51:48 -0300

    Add socket server

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ const mongoose = require('mongoose');
 
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect('mongodb+srv://twitter-user:58C22bb794uZ78V@cluster0-pnv1w.mongodb.net/test?retryWrites=true', {
     useNewUrlParser: true
 });
 
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
+
 app.use(express.json());
 app.use(require('./routes'));
 
-app.listen(3000,() => {
+server.listen(3000,() => {
     console.log('Server started on port 3000');
 });
