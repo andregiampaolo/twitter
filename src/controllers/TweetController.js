dbcf0914fd615504bf723e443b5353caf45d29c0
André Giampaolo <andregiampaolo@gmail.com>
2019-04-14 15:58:30 -0300

    Change namesof routes

--- src/controllers/TweetController.js
+++ src/controllers/TweetController.js
@@ -1,12 +1,12 @@
 const Tweet = require('../models/Tweet');
 
 module.exports = {
-    async index(req, res) {
+    async list(req, res) {
         const tweets = await Tweet.find({}).sort('-createdAt');
         return res.json(tweets);
     },
 
-    async store(req, res) {
+    async create(req, res) {
         const tweet = await Tweet.create(req.body);
         req.io.emit('tweet', tweet);
         return res.json(tweet);
