10093137f5c3bf505e94912f273a55ccca8f2ffc
André Giampaolo <andregiampaolo@gmail.com>
2019-12-16 00:27:49 -0300

    Create login route

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,5 +4,6 @@ const UserController = require('../controllers/UserController');
 
 routes.get('/list', UserController.list);
 routes.get('/signin', UserController.signUp);
+routes.post('/login', UserController.login);
 
 module.exports = routes;
